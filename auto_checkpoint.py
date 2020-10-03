709624eecc6f1398e0c51e2d963a60d1c9a7955a
Zhanrui Liang <ray040123@gmail.com>
2020-10-02 20:44:01 -0700

    v0.1

--- auto_checkpoint.py
+++ auto_checkpoint.py
@@ -1,8 +1,11 @@
 """For the ones that are too lazy to use git manually."""
 
 from typing import Optional
+import argparse
 import datetime
+import logging
 import os
+import sys
 import time
 
 import git
@@ -14,15 +17,15 @@ def watch(path: str, init_if_not_exist: bool = True, interval_secs: int = 120):
     repo = get_repo(path, init_if_not_exist)
     while True:
         try:
-            for f in repo.untracked_files:
-                if should_track(f):
-                    "git add f"
-            if repo.index.entries:
+            files_to_track = list(filter(should_track, repo.untracked_files))
+            repo.index.add(files_to_track)
+            if repo.index.diff(None):
                 msg = make_commit_message()
-                "git commit -m msg"
-        except:
-            pass
-
+                repo.git.add('-u')
+                repo.index.commit(msg)
+                logging.info('New commit made: %s', msg)
+        except Exception as e:
+            logging.error(e)
         time.sleep(interval_secs)
 
 def get_repo(path: str, init_if_not_exist=True) -> git.Repo:
@@ -30,6 +33,7 @@ def get_repo(path: str, init_if_not_exist=True) -> git.Repo:
         return git.Repo(path)
     except git.InvalidGitRepositoryError:
         if init_if_not_exist:
+            logging.info('Initializing new repo...')
             return git.Repo.init(path)
         raise UsageError(f'{path} is not a git directory')
 
@@ -45,3 +49,13 @@ def should_track(subpath: str) -> bool:
         if name.startswith('.') or name.startswith('__'):
             return False
     return True
+
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', metavar='PATH', type=str, help='The path to watch')
+    parser.add_argument('--interval', type=int, help='The refresh interval in seconds')
+    args = parser.parse_args(argv)
+    watch(args.path, interval_secs=args.interval)
+
+if __name__ == '__main__':
+    main(sys.argv)
