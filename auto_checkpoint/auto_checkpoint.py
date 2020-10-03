1472ae25fb85e2433d907b05d3954338da14e048
Zhanrui Liang <ray040123@gmail.com>
2020-10-02 23:16:42 -0700

    fix deadloop bug

--- auto_checkpoint/auto_checkpoint.py
+++ auto_checkpoint/auto_checkpoint.py
@@ -31,10 +31,8 @@ def watch(path: str, interval_secs: int, init_if_not_exist: bool = True):
         LOG.info('Scanning...')
         updated = False
         try:
-            files_to_track = list(filter(should_track, repo.untracked_files))
-            repo.index.add(files_to_track)
-            if repo.index.diff(None):
-                repo.git.add('-u')
+            repo.git.add('-u')
+            repo.git.add('.')
             if not has_commit(repo) or repo.index.diff('HEAD'):
                 msg = make_commit_message()
                 repo.index.commit(msg)
@@ -60,14 +58,6 @@ def make_commit_message(now: Optional[datetime.datetime] = None):
     if not now:
         now = datetime.datetime.now()
     return now.strftime('checkpoint at %Y-%m-%d %H:%M:%S')
-
-def should_track(subpath: str) -> bool:
-    dir_ = subpath
-    while dir_:
-        dir_, name = os.path.split(subpath)
-        if name.startswith('.') or name.startswith('__'):
-            return False
-    return True
 
 def has_commit(repo: git.Repo) -> bool:
     try:
--- tests/auto_checkpoint_test.py
+++ tests/auto_checkpoint_test.py
@@ -9,5 +9,8 @@ class AutoCheckpointTest(unittest.TestCase):
         now = datetime.datetime.strptime('2020-02-20 18:12:34', '%Y-%m-%d %H:%M:%S')
         self.assertEqual(auto_checkpoint.make_commit_message(now), 'checkpoint at 2020-02-20 18:12:34')
 
+    def test_should_check(self):
+        self.assertEqual(auto_checkpoint.should_track('/a/b/c'), True)
+
 if __name__ == '__main__':
     unittest.main()
